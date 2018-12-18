a19a32a43b78284eae0b0f2ed25b1abfa9845189
Unknown <skyvolga@mail.ru>
2018-12-18 20:20:18 +1300

    Skills were added

---
+++ GRS/GRS.UnitTests/Controller/SkillControllerTest.cs
@@ -0,0 +1,271 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GRS.UnitTests.Common;
+using GRS.Web.Controllers;
+using GRS.Web.Data;
+using GRS.Web.Data.Models;
+using GRS.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Update;
+using Moq;
+using NUnit.Framework;
+
+namespace GRS.UnitTests.Controller
+{
+    [TestFixture]
+    public class SkillControllerTest
+    {
+        [Test]
+        public async Task Index_SingleSkill_IsShown()
+        {
+            var contextCreator = Helper.InMemoryContextCreator();
+
+            using (var context = contextCreator())
+            {
+                await context.AddAsync(new Skill
+                {
+                    Name = "TestName",
+                });
+
+                await context.SaveChangesAsync();
+            }
+
+            var controller = new SkillController(contextCreator());
+
+            var result = (await controller.Index()) as ViewResult;
+            var model = (List<SkillViewModel>)result.Model;
+
+            Assert.AreEqual("TestName", model.Single().Name);
+        }
+
+        [Test]
+        public async Task Details_SingleSkill_IsShown()
+        {
+            var contextCreator = Helper.InMemoryContextCreator();
+
+            using (var context = contextCreator())
+            {
+                await context.AddAsync(new Skill
+                {
+                    Id = 1,
+                    Name = "TestName",
+                });
+
+                await context.SaveChangesAsync();
+            }
+
+            var controller = new SkillController(contextCreator());
+
+            var result = (await controller.Details(1)) as ViewResult;
+            var model = (SkillViewModel)result.Model;
+
+            Assert.AreEqual("TestName", model.Name);
+        }
+
+        [Test]
+        public async Task Details_IdIsNull_RederedtToIndex()
+        {
+            var contextCreator = Helper.InMemoryContextCreator();
+
+            var controller = new SkillController(contextCreator());
+
+            var result = (await controller.Details(null)) as NotFoundResult;
+
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public async Task Details_InvalidId_RederedtToIndex()
+        {
+            var contextCreator = Helper.InMemoryContextCreator();
+
+            var controller = new SkillController(contextCreator());
+
+            var result = (await controller.Details(1)) as NotFoundResult;
+
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public async Task Edit_SingleSkill_IsShown()
+        {
+            var contextCreator = Helper.InMemoryContextCreator();
+
+            using (var context = contextCreator())
+            {
+                await context.AddAsync(new Skill
+                {
+                    Id = 1,
+                    Name = "TestName",
+                });
+
+                await context.SaveChangesAsync();
+            }
+
+            var controller = new SkillController(contextCreator());
+
+            var result = (await controller.Edit(1)) as ViewResult;
+            var model = (SkillViewModel)result.Model;
+
+            Assert.AreEqual("TestName", model.Name);
+        }
+
+        [Test]
+        public async Task Edit_IdIsNull_RederedtToIndex()
+        {
+            var contextCreator = Helper.InMemoryContextCreator();
+
+            var controller = new SkillController(contextCreator());
+
+            var result = (await controller.Edit(null)) as NotFoundResult;
+
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public async Task Edit_InvalidId_RederedtToIndex()
+        {
+            var contextCreator = Helper.InMemoryContextCreator();
+
+            var controller = new SkillController(contextCreator());
+
+            var result = (await controller.Edit(1)) as NotFoundResult;
+
+            Assert.NotNull(result);
+        }
+
+
+        [Test]
+        public async Task EditPost_SingleSkill_IsShown()
+        {
+            var contextCreator = Helper.InMemoryContextCreator();
+
+            using (var context = contextCreator())
+            {
+                await context.AddAsync(new Skill
+                {
+                    Id = 1,
+                    Name = "TestName",
+                });
+
+                await context.SaveChangesAsync();
+            }
+
+            var controller = new SkillController(contextCreator());
+
+            var result = (await controller.Edit(1, new SkillViewModel { Id = 1 })) as RedirectToActionResult;
+
+            Assert.AreEqual(nameof(SkillController.Index), result.ActionName);
+        }
+
+        [Test]
+        public async Task EditPost_IdIsNotConsistent_RederedtToIndex()
+        {
+            var contextCreator = Helper.InMemoryContextCreator();
+
+            var controller = new SkillController(contextCreator());
+
+            var result = (await controller.Edit(1, new SkillViewModel { Id = 2 })) as NotFoundResult;
+
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public async Task EditPost_ModelStateInvalid_RederedtToIndex()
+        {
+            var contextCreator = Helper.InMemoryContextCreator();
+
+            var controller = new SkillController(contextCreator());
+            controller.ModelState.AddModelError("TetsError", "TetsErrorMessage");
+
+            var model = new SkillViewModel { Id = 1 };
+            var result = (await controller.Edit(1, model)) as ViewResult;
+            var resultModel = (SkillViewModel)result.Model;
+
+            Assert.AreSame(model, resultModel);
+        }
+
+        [Test]
+        public void EditPost_DbUpdateConcurrencyExceptionAndSkillExists_ThrowException()
+        {
+            var contextMock = new Mock<ApplicationDbContext>();
+            contextMock.Setup(x => x.Skills).Returns(Helper.GetQueryableMockDbSet(new Skill { Id = 1 }));
+            contextMock.Setup(x => x.Entry(It.IsAny<object>()))
+            .Throws(new DbUpdateConcurrencyException("exception",
+                 new List<IUpdateEntry>() { new Mock<IUpdateEntry>().Object }));
+
+            var controller = new SkillController(contextMock.Object);
+
+            Assert.ThrowsAsync<DbUpdateConcurrencyException>(async () =>
+            {
+                var result = await controller.Edit(1, new SkillViewModel { Id = 1 });
+            });
+        }
+
+        [Test]
+        public async Task EditPost_DbUpdateConcurrencyExceptionAnd_NOT_SkillExists_NotFoundIsShown()
+        {
+            var contextMock = new Mock<ApplicationDbContext>();
+            contextMock.Setup(x => x.Skills).Returns(Helper.GetQueryableMockDbSet(new Skill { Id = 2 }));
+            contextMock.Setup(x => x.Entry(It.IsAny<object>()))
+            .Throws(new DbUpdateConcurrencyException("exception",
+                 new List<IUpdateEntry>() { new Mock<IUpdateEntry>().Object }));
+
+            var controller = new SkillController(contextMock.Object);
+
+            var result = (await controller.Edit(1, new SkillViewModel { Id = 1 })) as NotFoundResult;
+
+            Assert.NotNull(result);
+        }
+
+
+        [Test]
+        public async Task Delete_SingleSkill_Deleted()
+        {
+            var contextCreator = Helper.InMemoryContextCreator();
+
+            using (var context = contextCreator())
+            {
+                await context.AddAsync(new Skill
+                {
+                    Id = 1,
+                    Name = "TestName",
+                });
+
+                await context.SaveChangesAsync();
+            }
+
+            var controller = new SkillController(contextCreator());
+
+            var result = (await controller.Delete(1)) as RedirectToActionResult;
+
+            Assert.AreEqual(nameof(SkillController.Index), result.ActionName);
+        }
+
+        [Test]
+        public async Task Delete_IdIsNull_RederedtToIndex()
+        {
+            var contextCreator = Helper.InMemoryContextCreator();
+
+            var controller = new SkillController(contextCreator());
+
+            var result = (await controller.Details(null)) as NotFoundResult;
+
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public async Task Delete_InvalidId_RederedtToIndex()
+        {
+            var contextCreator = Helper.InMemoryContextCreator();
+
+            var controller = new SkillController(contextCreator());
+
+            var result = (await controller.Details(1)) as NotFoundResult;
+
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ GRS/GRS.Web/Controllers/SkillController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using GRS.Web.Common;
+using GRS.Web.Data;
+using GRS.Web.Data.Models;
+using GRS.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GRS.Web.Controllers
+{
+    public class SkillController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SkillController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Skill
+        public async Task<IActionResult> Index()
+        {
+            var skills = await _context.Skills.ToListAsync();
+            var viewModel = skills.Select(x => new SkillViewModel(x)).ToList();
+            return View(viewModel);
+        }
+
+        // GET: Skill/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var model = await GetSkillAsync(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new SkillViewModel(model);
+            return View(viewModel);
+        }
+
+        // GET: Skill/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Skill/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SkillViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                await UpdateSkillAsync(_context, viewModel);
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(viewModel);
+        }
+
+        // GET: Skill/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var model = await GetSkillAsync(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new SkillViewModel(model);
+            return View(viewModel);
+        }
+
+        // POST: Skill/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, SkillViewModel viewModel)
+        {
+            if (id != viewModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await UpdateSkillAsync(_context, viewModel);
+
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SkillExists(viewModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(viewModel);
+        }
+
+        // GET: Skill/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var model = await GetSkillAsync(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            _context.Skills.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SkillExists(int? id)
+        {
+            return _context.Skills.Any(e => e.Id == id);
+        }
+
+        public async Task<Skill> UpdateSkillAsync(
+        ApplicationDbContext context,
+        SkillViewModel skillViewModel
+        )
+        {
+            var skill = (await context.Skills.FindAsync(skillViewModel?.Id)) ?? new Skill();
+
+            skill.Name = skillViewModel.Name;
+
+            context.AddOrUpdate(skill);
+
+            return skill;
+        }
+
+        private Task<Skill> GetSkillAsync(int? id)
+        {
+            return _context.Skills
+                            .SingleOrDefaultAsync(m => m.Id == id);
+        }
+    }
+}
